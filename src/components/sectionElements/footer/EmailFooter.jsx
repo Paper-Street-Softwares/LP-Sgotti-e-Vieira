e4838f927159ba373fa26fe85144fbbd5ee74bb6
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-09-15 09:09:30 -0300

    fix: Copys atualizados

--- src/components/sectionElements/footer/EmailFooter.jsx
+++ src/components/sectionElements/footer/EmailFooter.jsx
@@ -4,27 +4,29 @@ import MotionDivDownToUp from "../../animation/MotionDivDownToUp";
 import content from "../../../content/content";
 
 function EmailFooter({
-  emailTwo = false,
+  emailTwo = true,
   emailThree = false,
   emailFour = false,
   emailFive = false,
 }) {
   return (
     <>
-      <MotionDivDownToUp>
+      {/* <MotionDivDownToUp>
         <div className="flex full gap-x-[12px] items-center opacity-90">
           <Mail />
 
           <p className="text-paragraph2">{content.texts.infos.email}</p>
         </div>
-      </MotionDivDownToUp>
+      </MotionDivDownToUp> */}
 
       {emailTwo && (
         <MotionDivDownToUp>
           <div className="flex full gap-x-[12px] items-center opacity-90">
             <Mail />
 
-            <p className="text-paragraph2">{content.texts.infos.emailSecundario} </p>
+            <p className="text-paragraph2">
+              {content.texts.infos.emailSecundario}{" "}
+            </p>
           </div>
         </MotionDivDownToUp>
       )}
